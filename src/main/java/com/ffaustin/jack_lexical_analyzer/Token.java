3f636935a69f612c929c788723160516159d8758
Fabrice Faustin <fabricefaustin17@gmail.com>
2025-04-10 09:46:23 -0500

    refactor: changed Token to a record for cleaner code in accordance with Java 21.

--- src/main/java/com/ffaustin/jack_lexical_analyzer/Token.java
+++ src/main/java/com/ffaustin/jack_lexical_analyzer/Token.java
@@ -1,43 +1,27 @@
 package com.ffaustin.jack_lexical_analyzer;
 
-public class Token {
+public record Token(com.ffaustin.jack_lexical_analyzer.Token.Type type, String value) {
 
-    public enum Type{
+    public enum Type {
         KEYWORD, SYMBOL, IDENTIFIER, INT_CONST, STRING_CONST, INVALID
     }
 
-    private final Type type;
-    private final String value;
-
-    public Token(Type type, String value){
-        this.type = type;
-        this.value = value;
-    }
-
-    public Type getType(){
-        return type;
-    }
-
-    public String getValue(){
-        return value;
-    }
-
-    public String escapeXML(String input){
+    public String escapeXML(String input) {
         return input.replace("&", "&amp;")
                 .replace("<", "&lt;")
                 .replace(">", "&gt;");
     }
 
-    public String toXML(){
+    public String toXML() {
         String escapedValue = escapeXML(value);
 
-        switch(type){
-            case KEYWORD: return "\t<keyword> " + escapedValue + " </keyword>\n";
-            case SYMBOL: return "\t<symbol> " + escapedValue + " </symbol>\n";
-            case IDENTIFIER: return "\t<identifier> " + escapedValue + " </identifier>\n";
-            case INT_CONST: return "\t<integerConstant> " + escapedValue + " </integerConstant>\n";
-            case STRING_CONST: return "\t<stringConstant> " + escapedValue + " </stringConstant>\n";
-            default: return "\t<invalid> " + escapedValue + " </invalid>\n";
-        }
+        return switch (type) {
+            case KEYWORD -> "\t<keyword> " + escapedValue + " </keyword>\n";
+            case SYMBOL -> "\t<symbol> " + escapedValue + " </symbol>\n";
+            case IDENTIFIER -> "\t<identifier> " + escapedValue + " </identifier>\n";
+            case INT_CONST -> "\t<integerConstant> " + escapedValue + " </integerConstant>\n";
+            case STRING_CONST -> "\t<stringConstant> " + escapedValue + " </stringConstant>\n";
+            default -> "\t<invalid> " + escapedValue + " </invalid>\n";
+        };
     }
 }
